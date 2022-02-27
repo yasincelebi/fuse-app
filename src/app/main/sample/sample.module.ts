8e1e8664a031e58d51e0f4b70f5949a8eb5a2f50
yasincelebi <yasincelebix@gmail.com>
2022-02-27 18:42:01 +0300

    add mat modules

--- src/app/main/sample/sample.module.ts
+++ src/app/main/sample/sample.module.ts
@@ -5,11 +5,25 @@ import { TranslateModule } from '@ngx-translate/core';
 import { FuseSharedModule } from '@fuse/shared.module';
 
 import { SampleComponent } from './sample.component';
+import {CreateContactComponent} from "../create-contact/create-contact.component";
+import {MatTableModule} from "@angular/material/table";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
 
 const routes = [
     {
         path     : 'sample',
         component: SampleComponent
+    },
+    {
+        path: 'frequently',
+        component: SampleComponent
+    },
+    {
+        path: 'new',
+        component: CreateContactComponent
     }
 ];
 
@@ -17,12 +31,17 @@ const routes = [
     declarations: [
         SampleComponent
     ],
-    imports     : [
+    imports: [
         RouterModule.forChild(routes),
 
         TranslateModule,
 
-        FuseSharedModule
+        FuseSharedModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatListModule,
+        MatIconModule,
+        MatMenuModule
     ],
     exports     : [
         SampleComponent
